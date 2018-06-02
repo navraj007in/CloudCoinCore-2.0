96ede84ae0ea665774e004d332dc204b2689464a
Sean Worthington <sean@worthington.net>
2018-06-02 15:50:55 -0700

    Added getMultiDetectionRequest Method
    Hopefully this does not screw anything up.

--- CloudCoinCoreStandard/CloudCoinCoreStandard/Stack.cs
+++ CloudCoinCoreStandard/CloudCoinCoreStandard/Stack.cs
@@ -5,6 +5,11 @@ namespace CloudCoinCore
 {
     public class Stack
     {
+        /* Properties */
+       [JsonProperty("cloudcoin")]
+        public CloudCoin[] cc { get; set; }
+        
+        /* Constructors */
         public Stack()
         {
 
@@ -22,7 +27,27 @@ namespace CloudCoinCore
         {
             cc = coins;
         }
-        [JsonProperty("cloudcoin")]
-        public CloudCoin[] cc { get; set; }
+
+        /* Methods */ 
+        
+         public MultiDetectRequest getMultiDetectRequest(int timeout) {  //Method for Extracting a MultiDetection Request from stack file
+            
+            MultiDetectRequest returnRequest = new MultiDetectRequest();
+            //Create the arrays for the detection request
+            returnRequest.nn = new int[cc.Length];
+            returnRequest.sn = new int[cc.Length];
+            returnRequest.an = new string[cc.Length][];
+            returnRequest.pan = new string[cc.Length][];
+            returnRequest.timeout = timeout;
+            
+            for ( int i=0; i< cc.Length; i++) {
+                returnRequest.nn[i] = cc[i].nn;
+                returnRequest.sn[i] = cc[i].sn;
+                returnRequest.an[i] = cc[i].an;
+                returnRequest.pan[i] = cc[i].pan;
+            }//end for each CloudCoin in the stack
+
+            return returnRequest;
+        }//end get multi detect request
     }
 }
